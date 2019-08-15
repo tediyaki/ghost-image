561af950017a6b71c935df81857d5a68fc82071a
tediyaki <iskandar.teddy93@gmail.com>
2019-08-15 22:00:53 +0700

    add send file client

--- client/components/form.vue
+++ client/components/form.vue
@@ -1,25 +1,45 @@
 <template>
-    <form>
-        <label for="title" v-model="title">Title</label>
-        <input type="text" name="title" required>
-        <label for="file">File</label>
-        <input type="file" name="image">
-        <label for="description" v-model="description">Description</label>
-        <textarea rows="4" name="description">
-        <input type="submit" value="Submit">
+    <form @submit="sendForm">
+        <label for="title" v-model="title">Title</label><br>
+        <input v-model="title" type="text" name="title" required><br>
+        <label for="file">File</label><br>
+        <input @change="imageUpload" ref="file" type="file" name="image"><br>
+        <input type="submit" value="Submit"><br>
     </form>
 </template>
 
 <script>
     export default {
-        export default {
-            data() {
-                return {
-                    title: '',
-                    description: '',
-                };
+        data() {
+            return {
+                title: '',
+                file: ''
+            };
+        },
+        methods: {
+            imageUpload() {
+                this.file = this.$refs.file.files[0]
+                console.log(this.file)
             },
-        };
+            sendForm() {
+                const formData = new FormData()
+                formData.append('title', this.title)
+                formData.append('file', this.file);
+                axios.post( '/image',
+                    formData,
+                    {
+                        headers: {
+                            'Content-Type': 'multipart/form-data'
+                        }
+                    }
+                ).then(function(){
+                    console.log('SUCCESS!!');
+                })
+                .catch(function(){
+                    console.log('FAILURE!!');
+                });
+            }
+        }
     }
 </script>
 
