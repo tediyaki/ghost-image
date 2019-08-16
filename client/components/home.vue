cfd728ac3c8fed006ae9ae86e6d77ef59bbca985
tediyaki <iskandar.teddy93@gmail.com>
2019-08-16 13:22:31 +0700

    switch branch

---
+++ client/components/home.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="content"
+        <div class="grid" v-for="(image, index) in images">
+            <figure class="effect-lily">
+                <img v-bind:src="image.link" alt="img12"/>
+                <figcaption>
+                    <div>
+                        <div class="fb-share-button" 
+                        v-bind:data-href="image.link"
+                        data-layout="button_count">
+                        </div> <br>
+                        <a  v-bind:href="'https://twitter.com/intent/tweet?text='+image.link"  class="twitter-share-button" data-hashtags="Gambar Seram">Tweet</a>
+                        <h2> {{ image.title }} </h2>
+                    </div>
+                </figcaption>           
+            </figure>
+        <div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Home",
+    data() {
+        return {
+            images: [],
+        }
+    },
+
+    methods: {
+        showImage() {
+            axios.get('http://localhost:3000/ghost/')
+            .then(({data})=> {
+                console.log("=============")
+                this.images = data.data
+                console.log(this.images)
+            })
+            .catch((err)=> {
+                console.log(err)
+            })
+        }
+    },
+    created() {
+        this.showImage()
+    }
+}
+</script>
+
+<style>
+    .content {
+        color: white;
+        text-align: center;
+    }
+</style>
+ 
